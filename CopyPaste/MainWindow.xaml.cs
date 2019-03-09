1f42383bd840e8563497fece3e38c4fcd48767ad
Dmitry Didenko <dmytro.didenko@logika.tech>
2019-03-09 20:38:32 +0200

    first commit

---
+++ CopyPaste/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace CopyPaste {
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() { InitializeComponent(); }
+	}
+}
